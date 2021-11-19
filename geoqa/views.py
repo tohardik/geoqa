22c106afd626142c4f599fdd4cfe155e037ebd77
toardik <hardik.topiwala@gmail.com>
2021-11-19 21:55:04 +0100

    empty answer for exception

--- geoqa/views.py
+++ geoqa/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request, jsonify
 from geoqa import app
 from geoqa.service.orchestration import Orchestrator
 
-orchestration_service = Orchestrator()
+from geoqa import app as flask_app
 
 
 @app.route("/")
@@ -14,16 +14,29 @@ def index():
 
 @app.route("/qa", methods=["POST", "GET"])
 def qa():
-    if request.method == "POST":
-        query = request.form.get('query')
-        lang = request.form.get('lang', 'en')
-    else:
-        query = request.args.get('query')
-        lang = request.args.get('lang', 'en')
+    try:
+        if request.method == "POST":
+            query = request.form.get('query')
+            lang = request.form.get('lang', 'en')
+        else:
+            query = request.args.get('query')
+            lang = request.args.get('lang', 'en')
 
-    orc = Orchestrator()
-    answer = orc.answer_question(query, lang)
-    return jsonify(get_qald_format_answer(answer))
+        flask_app.logger.info(f"{request.method} /qa {query}")
+
+        orchestration_service = Orchestrator()
+        answer = orchestration_service.answer_question(query, lang)
+        return jsonify(get_qald_format_answer(answer))
+    except Exception as e:
+        flask_app.logger.error(f"Error: {str(e)}")
+        return jsonify({
+            "head": {
+                "vars": ["x"]
+            },
+            "results": {
+                "bindings": []
+            }
+        })
 
 
 def get_qald_format_answer(answer):
